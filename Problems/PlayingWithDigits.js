a4a84667ec9c7f615c0672a3bf861a3dc68cc764
JacobMooney <Jacobhmoon@gmail.com>
2022-03-14 00:36:02 -0500

    Playing with digits problem

---
+++ Problems/PlayingWithDigits.js
@@ -0,0 +1,44 @@
+// https://www.codewars.com/kata/5552101f47fc5178b1000050
+
+// Some numbers have funny properties. For example:
+
+// 89 --> 8¹ + 9² = 89 * 1
+
+// 695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
+
+// 46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+
+// Given a positive integer n written as abcd... (a, b, c, d... being digits) and a positive integer p
+
+// we want to find a positive integer k, if it exists, such that the sum of the digits of n taken to the successive powers of p is equal to k * n.
+// In other words:
+
+// Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k
+
+// If it is the case we will return k, if not return -1.
+
+// Note: n and p will always be given as strictly positive integers.
+
+// digPow(89, 1) should return 1 since 8¹ + 9² = 89 = 89 * 1
+// digPow(92, 1) should return -1 since there is no k such as 9¹ + 2² equals 92 * k
+// digPow(695, 2) should return 2 since 6² + 9³ + 5⁴= 1390 = 695 * 2
+// digPow(46288, 3) should return 51 since 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+
+function digPow(nums, power) {
+  //split number into an array of numbers
+  let numbers = ("" + nums).split(""); //?
+  let numSum = numbers.reduce((sum, number, index) => {
+    //raise each digit to the power of the provided power, increasing with each new digit
+    sum += Math.pow(number, power+index);
+    return sum;
+  }, 0); //?
+  //divide the reduced value by the initial argument
+  let funProp = numSum/nums;
+  //if the above value has any remainder or is less than 1, then we know the number does not return a single int like we want so return -1
+   //else return the int from funProp
+  return funProp % 1 !== 0 ? -1 : funProp;
+}
+
+console.log(digPow(89, 1), 1);
+console.log(digPow(92, 1), -1);
+console.log(digPow(46288, 3), 51);
